8a3770b3df0ba6c8524a9d8fdc28827be605b368
Igor Valet <igor_valet@hotmail.com>
2018-10-13 01:07:16 +0200

    [dumb/server] Simplified + formatted messages

--- src/dumb/server.rs
+++ src/dumb/server.rs
@@ -1,38 +1,32 @@
 extern crate futures;
 extern crate tokio;
 
-use tokio::prelude::*;
-
 use futures::{future, Future, Stream};
 
 fn main() {
     let port = 12345;
-    tokio::run(future::poll_fn(move || {
-        let addr = format!("127.0.0.1:{}", port).parse().unwrap();
-        let listener = tokio::net::TcpListener::bind(&addr).unwrap();
-        tokio::spawn(
-            listener
-                .incoming()
-                .for_each(|socket| {
-                    println!("accepted socket; addr={:?}", socket.peer_addr().unwrap());
+    let addr = format!("127.0.0.1:{}", port).parse().unwrap();
+    let listener = tokio::net::TcpListener::bind(&addr).unwrap();
 
-                    let stream = tokio::codec::Framed::new(socket, tokio::codec::LinesCodec::new());
-                    tokio::spawn(
-                        stream
-                            .for_each(|msg| {
-                                println!("Client said '{}'", msg);
-                                future::ok(())
-                            }).map_err(|err| {
-                                println!("socket error = {:?}", err);
-                            }),
-                    );
-                    future::ok(())
-                }).map_err(|err| {
-                    println!("accept error = {:?}", err);
-                }),
-        );
+    tokio::run(
+        listener
+            .incoming()
+            .for_each(|socket| {
+                println!("New client from {:?}", socket.peer_addr().unwrap());
 
-        println!("server running on localhost:{}", port);
-        Ok(Async::Ready(()))
-    }));
+                let stream = tokio::codec::Framed::new(socket, tokio::codec::LinesCodec::new());
+                tokio::spawn(
+                    stream
+                        .for_each(|msg| {
+                            println!("Client said: '{}'", msg);
+                            future::ok(())
+                        }).map_err(|err| {
+                            println!("Socket error: {:?}", err);
+                        }),
+                );
+                future::ok(())
+            }).map_err(|err| {
+                println!("Accept error: {:?}", err);
+            }),
+    );
 }
