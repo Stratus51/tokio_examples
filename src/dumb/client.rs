545d9dc35e1a943c7454078c72f8b2111b7a993e
Igor Valet <igor_valet@hotmail.com>
2018-10-13 00:53:20 +0200

    Cargo fmt

--- src/dumb/client.rs
+++ src/dumb/client.rs
@@ -6,19 +6,22 @@ use futures::{future, Future, Stream};
 fn main() {
     let port = 12345;
     let addr = format!("127.0.0.1:{}", port).parse().unwrap();
-    tokio::run(tokio::net::TcpStream::connect(&addr).and_then(|socket| {
-        let stream = tokio::codec::Framed::new(socket, tokio::codec::LinesCodec::new());
-        tokio::spawn(
-            stream
-                .for_each(|msg| {
-                    println!("Client said '{}'", msg);
-                    future::ok(())
-                }).map_err(|err| {
-                    println!("socket error = {:?}", err);
-                }),
-        );
-        future::ok(())
-    }).map_err(|err| {
-        println!("connect error = {:?}", err);
-    }));
+    tokio::run(
+        tokio::net::TcpStream::connect(&addr)
+            .and_then(|socket| {
+                let stream = tokio::codec::Framed::new(socket, tokio::codec::LinesCodec::new());
+                tokio::spawn(
+                    stream
+                        .for_each(|msg| {
+                            println!("Client said '{}'", msg);
+                            future::ok(())
+                        }).map_err(|err| {
+                            println!("socket error = {:?}", err);
+                        }),
+                );
+                future::ok(())
+            }).map_err(|err| {
+                println!("connect error = {:?}", err);
+            }),
+    );
 }
