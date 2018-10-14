070a0a80d5ef24fe369ca28a85a95d5cfdfd813e
Igor Valet <igor_valet@hotmail.com>
2018-10-14 10:18:44 +0200

    [dumb/client] Added comments

--- src/dumb/client.rs
+++ src/dumb/client.rs
@@ -1,7 +1,10 @@
 extern crate futures;
 extern crate tokio;
 
-use futures::{Future};
+// Trait required for map_err
+use futures::Future;
+
+// Trait required for stream send method
 use futures::sink::Sink;
 
 fn main() {
@@ -13,26 +16,40 @@ fn main() {
     tokio::run(
         // Creating a ConnectFuture (attempt to connect)
         tokio::net::TcpStream::connect(&addr)
+
+            // Processing connect error.
+            // -------------------------
+            // Has to be before and_then or map_err will then process and_then
+            // errors. It might seem obvious to many, but I lost 10 mins thinking
+            // It was about a type issue so I prefer underlining it.
             .map_err(|err| {
                 println!("Connect error: {:?}", err);
             })
-            // Processing connect success: that is what and_then (from Future trait) is for
+
+            // Processing connect success
+            // --------------------------
+            // That is what and_then (from Future trait) is for
             .and_then(|socket| {
                 // Creating a stream from our socket (see server for details)
                 let stream = tokio::codec::Framed::new(socket, tokio::codec::LinesCodec::new());
 
+                // Sending a hello message
                 stream.send("I am a client".to_string()).map_err(|err| {
+                    // Processing errors (before and_then)
                     println!("Send error: {:?}", err);
+                // On success, do:
                 }).and_then(|mut stream| {
                     println!("Successfully sent packet!");
+
+                    // Close socket
                     match stream.close() {
                         Ok(_) => println!("Successfully closed the stream !"),
                         Err(e) => println!("Couldn't close the socket: {:?}", e),
                     };
+
+                    // Return required next future (which does nothing successfully)
                     futures::future::ok(())
                 })
-
-            // Processing connect error
             }),
     );
 }
