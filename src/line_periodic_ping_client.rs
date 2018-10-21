6de147ee35093dd92d4ba37cb75442a3f62366c7
Igor Valet <igor_valet@hotmail.com>
2018-10-21 17:38:46 +0200

    [line_periodic_ping_client] Now properly implementing my own future

--- src/line_periodic_ping_client.rs
+++ src/line_periodic_ping_client.rs
@@ -1,6 +1,25 @@
 extern crate futures;
 extern crate tokio;
 
+// Error management boiler plate
+#[derive(Debug)] // For {:?} printability
+enum PingerError {
+    Socket(tokio::io::Error),
+    Timer(tokio::timer::Error),
+}
+
+impl std::convert::From<tokio::timer::Error> for PingerError {
+    fn from(data: tokio::timer::Error) -> Self {
+        PingerError::Timer(data)
+    }
+}
+
+impl std::convert::From<tokio::io::Error> for PingerError {
+    fn from(data: tokio::io::Error) -> Self {
+        PingerError::Socket(data)
+    }
+}
+
 // Trait required for map_err
 use futures::Future;
 
@@ -13,13 +32,42 @@ use futures::sink::Sink;
 // Standard time structures
 use std::time::{Duration, Instant};
 
+struct Pinger {
+    stream: tokio::codec::Framed<tokio::net::TcpStream, tokio::codec::LinesCodec>,
+    interval: tokio::timer::Interval,
+}
+
+impl futures::Future for Pinger {
+    type Item = ();
+    type Error = PingerError;
+
+    fn poll(&mut self) -> futures::Poll<Self::Item, Self::Error> {
+        while let futures::Async::Ready(data) = self.stream.poll()? {
+            match data {
+                Some(line) => println!("Server said: {}", line),
+                None => {
+                    println!("Socket closed. Exiting.");
+                    return Ok(futures::Async::Ready(()));
+                }
+            }
+        }
+
+        while let futures::Async::Ready(Some(_instant)) = self.interval.poll()? {
+            println!("Sending ping!");
+            self.stream.start_send("Ping".to_string())?;
+        }
+        self.stream.poll_complete()?;
+
+        Ok(futures::Async::NotReady)
+    }
+}
+
 fn main() {
     // Configuration
     let port = 12345;
     let addr = format!("127.0.0.1:{}", port).parse().unwrap();
     let ping_period = Duration::from_secs(1);
-    let ping_start_offset = 5;
-    let ping_start = Instant::now() + Duration::from_secs(ping_start_offset);
+    let ping_start = Instant::now() + Duration::from_secs(5);
 
     // Launching scheduler
     tokio::run(
@@ -39,73 +87,13 @@ fn main() {
             // --------------------------
             // That is what and_then (from Future trait) is for
             .and_then(move |socket| {
-                // Creating a stream from our socket (see server for details)
-                let stream = tokio::codec::Framed::new(socket, tokio::codec::LinesCodec::new());
-
-                println!("Waiting for {}s", ping_start_offset);
-
-                // Create our interval (periodic timer)
-                // ------------------------------------
-                // This interval will start at the time ping_start and then trigger
-                // every ping_period.
-                // It is also the return item of our closure which means it will
-                // be spawned by the scheduler automatically
-                tokio::timer::Interval::new(ping_start, ping_period)
-                    // Catch interval error
-                    .map_err(|err| {
-                        println!("Couldn't start the timer: {:?}", err);
-                    })
-                    // TODO this is a terrible hack totally unusable in practice.
-                    // ... Working on the simplest fix I can find. Right now the
-                    // only option I see is the one implemented in the official
-                    // tokio chat example. But it's a bit cumbersome so I'm digging
-                    // just in case there a simple correct way for this example.
-                    //
-                    // Okay, I needed to insert the stream somehow in the interval
-                    // closure, which was complicated as I could not just move it in
-                    // because of closure type conflict (something about FnOnce and
-                    // FnMut).
-                    //
-                    // Therefore, I ended up copying a trick I found on mqtt-protocol.
-                    //
-                    // I used the method fold that usually means on an iterator:
-                    // Start with a value and modify it with each item from the
-                    // iterator. Therefore on each new item, the closure will be passed
-                    // the current value and will have to return the new one.
-                    //
-                    // When the iterator is exhausted, the fold method will return the
-                    // last value.
-                    //
-                    // Here this is exactly what we do: we give the initial value to
-                    // fold (our stream), then it will be passed as a mutable to our
-                    // closure to be modified and at the end we have to return the
-                    // result. We return our stream unchanged so that it can be passed
-                    // again to us as the current value on the next turn.
-                    //
-                    // We're not really folding the values into one... Or if we
-                    // consider a stream filled with the wanted packets a result then
-                    // I guess we are folding something.
-                    // (Well I'm not even using the interval value but whatever)
-                    .fold(stream, |stream, _interval| {
-                        // Sending a ping message
-                        stream.send("Ping".to_string())
-                        // Processing send errors
-                        .map_err(|err| {
-                            println!("Send error: {:?}", err);
-                        // On success print
-                        }).and_then(|stream| {
-                            println!("Successfully pinged!");
-
-                            // Return required next future containing the fold result
-                            futures::future::ok(stream)
-                        })
-                    // The "final" future result has to be Result<(), ()> for
-                    // tokio::run to be execute successfully. Therefore we just drop
-                    // the folded value (our stream) to match the wanted type.
-                    //
-                    // We could have closed the stream. But you know, it'll be closed
-                    // anyway when droping... I assume ... :p
-                    }).map(|_stream| {})
+                Pinger {
+                    stream : tokio::codec::Framed::new(socket, tokio::codec::LinesCodec::new()),
+                    interval : tokio::timer::Interval::new(ping_start, ping_period),
+                }
+                .map_err(|err| {
+                    println!("Pinger error: {:?}", err);
+                })
             }),
     );
 }
